7fe64c15f0e50b7ea8a6a7c2e135a2ff58a26bdc
ajoshi <a.joshi@levi9.com>
2018-08-31 15:24:26 +0300

    [NSI] - added determinant calculation

--- matrix.go
+++ matrix.go
@@ -1,1 +1,51 @@
 package math
+
+// returns det of matrix if specified algebra
+func Det(matrix [][]byte, alg ByteAlgebra) byte {
+	size := len(matrix)
+	if size == 1 {
+		return matrix[0][0]
+	}
+	if size == 2 {
+		return det2(matrix, alg)
+	}
+	var det byte = 0
+	for i := 0; i < size; i++ {
+		element := matrix[0][i]
+		minor := Det(cross(matrix, 0, i), alg)
+		if i%2 == 0 {
+			det = alg.Add(det, alg.Mul(element, minor))
+		} else {
+			det = alg.Sub(det, alg.Mul(element, minor))
+		}
+	}
+	return det
+}
+
+// returns matrix with size n-1 crossing row and column specified
+func cross(matrix [][]byte, row, col int) [][]byte {
+	size := len(matrix)
+	result := make([][]byte, size-1)
+
+	var i, j, a, b int
+	for i, a = 0, 0; i < size; i++ {
+		if i == row {
+			continue
+		}
+		result[a] = make([]byte, size-1)
+		for j, b = 0, 0; j < size; j++ {
+			if j == col {
+				continue
+			}
+			result[a][b] = matrix[i][j]
+			b++
+		}
+		a++
+	}
+	return result
+}
+
+// returns det of 2x2 matrix
+func det2(matrix [][]byte, alg ByteAlgebra) byte {
+	return alg.Sub(alg.Mul(matrix[0][0], matrix[1][1]), alg.Mul(matrix[1][0], matrix[0][1]))
+}
---
+++ matrix_test.go
@@ -0,0 +1,82 @@
+package math
+
+import (
+	"testing"
+)
+
+var algebra ByteAlgebra = new(ByteAlgebraImpl)
+
+func TestDetZeroMatrix(t *testing.T) {
+	zeroMatrix := [][]byte{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	det3ZeroMatrix := Det(zeroMatrix, algebra)
+	if det3ZeroMatrix != 0 {
+		t.Errorf("Det of zero matrix must be 0 but it's %d", det3ZeroMatrix)
+	}
+}
+
+func TestDetOneMatrix(t *testing.T) {
+	oneMatrix := [][]byte{
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1},
+	}
+
+	det3OneMatrix := Det(oneMatrix, algebra)
+	if det3OneMatrix != 1 {
+		t.Errorf("Det of one matrix must be 1 but it's %d", det3OneMatrix)
+	}
+}
+
+func TestDetMatrix1(t *testing.T) {
+	m := [][]byte{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	det := Det(m, algebra)
+	if det != 0 {
+		t.Errorf("Det of matrix must be 0 but it's %d", det)
+	}
+}
+
+func TestDetMatrix2(t *testing.T) {
+	m := [][]byte{
+		{5, 5, 2},
+		{5, 3, 2},
+		{5, 5, 1},
+	}
+	det := Det(m, algebra)
+	if det != 10 {
+		t.Errorf("Det of matrix must be 10 but it's %d", det)
+	}
+}
+
+func TestDetMatrix3(t *testing.T) {
+	m := [][]byte{
+		{1, 5, 2, 4, 3},
+		{2, 5, 7, 8, 5},
+		{2, 6, 4, 8, 7},
+		{3, 5, 4, 1, 2},
+		{3, 5, 2, 4, 5},
+	}
+	det := Det(m, algebra)
+	if det != 198 {
+		t.Errorf("Det3 of case 198 matrix must be 0 but it's %d", det)
+	}
+}
+
+func TestCross(t *testing.T) {
+	matrix := [][]byte{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	cross := cross(matrix, 1, 1)
+	if cross[0][0] != 1 || cross[0][1] != 3 || cross[1][0] != 7 || cross[1][1] != 9 {
+		t.Error("Cross function returns wrong result")
+	}
+}
